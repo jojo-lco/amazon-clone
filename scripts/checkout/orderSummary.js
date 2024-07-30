e194c00665d132401c42e8805629f884ab95f67c
Josephine Wanjiru Ndungu <ndungujow@gmail.com>
2024-07-30 09:39:48 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary() {
@@ -117,6 +118,7 @@ export function renderOrderSummary() {
       container.remove();
 
       displayCartQuantity();
+      renderPaymentSummary();
     })
   })
 
@@ -167,6 +169,7 @@ export function renderOrderSummary() {
         updateDeliveryOption(productId, deliveryOptionId)
         
         renderOrderSummary();
+        renderPaymentSummary();
       })
     })
   }
