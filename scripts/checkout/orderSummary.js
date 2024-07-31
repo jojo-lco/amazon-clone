125d9eb2eac161197730b74a2d3b26d71ed3b437
Josephine Wanjiru Ndungu <ndungujow@gmail.com>
2024-07-31 10:05:23 +0300

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -23,47 +23,49 @@ export function renderOrderSummary() {
    const dateString = calculateDeliveryDate(deliveryOption);
 
   let html = `
-    <div class="cart-item-container 
-          js-cart-item-container-${matchingProduct.id}">
-              <div class="delivery-date">
-                Delivery date: ${dateString}
-              </div>
+    <div class="cart-item-container
+       js-cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
+        <div class="delivery-date">
+          Delivery date: ${dateString}
+        </div>
 
-              <div class="cart-item-details-grid">
-                <img class="product-image"
-                  src="${matchingProduct.image}">
+        <div class="cart-item-details-grid">
+          <img class="product-image"
+            src="${matchingProduct.image}">
 
-                <div class="cart-item-details">
-                  <div class="product-name">
-                    ${matchingProduct.name}
-                  </div>
-                  <div class="product-price">
-                    $${formatCurrency(matchingProduct.priceCents)}
-                  </div>
-                  <div class="product-quantity">
-                    <span>
-                      Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
-                    </span>
-                    <span class="update-quantity-link link-primary js-update-quality-link" data-product-id="${matchingProduct.id}">
-                      Update
-                    </span>
-                    <input class="quantity-input js-quantity-input">
-                    <span class="save-quantity-link link-primary js-save-quantity-link">Save
-                    </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                      Delete
-                    </span>
-                  </div>
-                </div>
-
-                <div class="delivery-options">
-                  <div class="delivery-options-title">
-                    Choose a delivery option:
-                  </div>
-                  ${deliveryOptionsHTML(matchingProduct, cartItem)}
-                </div>
-              </div>
+          <div class="cart-item-details">
+            <div class="product-name">
+              ${matchingProduct.name}
             </div>
+            <div class="product-price">
+              $${formatCurrency(matchingProduct.priceCents)}
+            </div>
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
+            <span>
+              Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary js-update-quality-link" data-product-id="${matchingProduct.id}">
+              Update
+            </span>
+            <input class="quantity-input js-quantity-input">
+            <span class="save-quantity-link link-primary js-save-quantity-link">Save
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" 
+            data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
+        </div>
+      </div>
+    </div>
     `
     cartSummaryHTML += html;
   })
