4f97a3de8f7ca916bdcfc8adac39d5b42cdfffdf
Josephine Wanjiru Ndungu <ndungujow@gmail.com>
2024-08-01 07:52:14 +0300

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
     products.forEach((product) => {
@@ -5,6 +7,29 @@ export function getProduct(productId) {
     })
 
     return matchingProduct;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsURL() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
 }
 
 
@@ -667,4 +692,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
