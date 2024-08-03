9f9a8aee278754fac2cd2af1134ef9422ee2a316
Josephine Wanjiru Ndungu <ndungujow@gmail.com>
2024-08-03 09:26:53 +0300

    Convert loadProducts from xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,14 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js';
 import { loadCart } from "../data/cart.js";
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    }) 
-  }),
+  loadProductsFetch(),
+  
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
