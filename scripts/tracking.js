f8110658f60579aa6f5d49fc894e7d961cc8e23c
Josephine Wanjiru Ndungu <ndungujow@gmail.com>
2024-08-05 09:24:45 +0300

    Display the progress bar with estimated time of delivery

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -18,6 +18,11 @@ async function renderTrackingPage() {
     if(details.productId === product.id) productDetails = details;
   })
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
   const trackingHTML = 
         `<a class="back-to-orders-link link-primary" href="orders. html">
           View all orders
@@ -38,19 +43,25 @@ async function renderTrackingPage() {
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress < 50 ? 'current-status' : ''
+          }">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${
+            (percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''
+          }">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress >= 100 ? "current-status" : ''
+          }">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar" style="width: ${percentProgress}%;"></div>
         </div>`
 
   document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
